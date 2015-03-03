e221aa90df6dc198f209b1632517b0eb13dbf891
jaspervl <hekbek@hotmail.nl>
2015-03-03 13:50:00 +0100

    asa
    ass

--- StudyOrDie/src/nl/glasbakheroes/studyordie/model/Game.java
+++ StudyOrDie/src/nl/glasbakheroes/studyordie/model/Game.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:StudyOrDie/src/nl/glasbakheroes/studyordie/model/Game.java
 package nl.glasbakheroes.studyordie.model;
 
 /**
@@ -25,3 +26,36 @@ public abstract class Game {
 		return gameBoard;
 	}
 }
+=======
+package nl.saxion.act.playground.model;
+
+/**
+ * Superclass for all games. 
+* 
+ * You should subclass this for your own game. In that subclass you will keep
+ * track of all game-related state, like the score, who's turn it is, etc.
+ * 
+ * BLAAAAAAAAAAAAAANKFNDKFNJKNDFKNDNFKNKDNFKNDKFNKNFKNDSKF
+ * gfjgfdgdfgjdfgjfdgj
+ * JASPER IS GEK
+ * 
+ * @author Paul de Groot
+ */
+public abstract class Game {
+	private GameBoard gameBoard;
+
+	/**
+	 * Called when you create a new game.
+	 * @param gameBoard
+	 */
+	public Game(GameBoard gameBoard) {
+		this.gameBoard = gameBoard;
+		gameBoard.setGame(this);
+	}
+
+	/** Returns a reference to the game board. */
+	public GameBoard getGameBoard() {
+		return gameBoard;
+	}
+}
+>>>>>>> parent of 8a265f9... jajaja:StudyOrDie/src/nl/saxion/act/playground/model/Game.java
