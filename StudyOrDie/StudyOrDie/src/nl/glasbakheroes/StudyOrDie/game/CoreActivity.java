1ee61b2179366ae6ee0a4c513723f65f9656fa4e
jaspervl <hekbek@hotmail.nl>
2015-03-03 14:29:18 +0100

    sad
    sad

---
+++ StudyOrDie/StudyOrDie/src/nl/glasbakheroes/StudyOrDie/game/CoreActivity.java
@@ -0,0 +1,16 @@
+package nl.glasbakheroes.StudyOrDie.game;
+
+import nl.glasbakheroes.StudyOrDie.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CoreActivity extends Activity {
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+}
