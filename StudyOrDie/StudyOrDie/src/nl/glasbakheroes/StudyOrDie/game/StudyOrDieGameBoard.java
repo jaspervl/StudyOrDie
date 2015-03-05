e8e61569a16c7fb11c75b1a79764e5893d2e0128
enjee <njvandepol@gmail.com>
2015-03-05 10:25:33 +0100

    minor change to SoDGB

--- StudyOrDie/StudyOrDie/src/nl/glasbakheroes/StudyOrDie/game/StudyOrDieGameBoard.java
+++ StudyOrDie/StudyOrDie/src/nl/glasbakheroes/StudyOrDie/game/StudyOrDieGameBoard.java
@@ -1,5 +1,18 @@
 package nl.glasbakheroes.StudyOrDie.game;
 
+import nl.glasbakheroes.StudyOrDie.model.GameBoard;
+
 public class StudyOrDieGameBoard extends GameBoard {
+
+	public StudyOrDieGameBoard(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onEmptyTileClicked(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
