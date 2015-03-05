3c057d4be27eb533022c01a1b192673160ea7456
enjee <njvandepol@gmail.com>
2015-03-05 10:30:20 +0100

    userStorieGameBoardCompleted

--- StudyOrDie/StudyOrDie/src/nl/glasbakheroes/StudyOrDie/game/StudyOrDieGameBoard.java
+++ StudyOrDie/StudyOrDie/src/nl/glasbakheroes/StudyOrDie/game/StudyOrDieGameBoard.java
@@ -16,3 +16,4 @@ public class StudyOrDieGameBoard extends GameBoard {
 	}
 
 }
+	
--- StudyOrDie/StudyOrDie/src/nl/glasbakheroes/StudyOrDie/game/StudyOrDieGameBoardView.java
+++ StudyOrDie/StudyOrDie/src/nl/glasbakheroes/StudyOrDie/game/StudyOrDieGameBoardView.java
@@ -1,5 +1,24 @@
 package nl.glasbakheroes.StudyOrDie.game;
 
-public class StudyOrDieGameBoardView {
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import nl.glasbakheroes.StudyOrDie.view.GameBoardView;
+
+public class StudyOrDieGameBoardView extends GameBoardView{
+
+	public StudyOrDieGameBoardView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	public StudyOrDieGameBoardView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init();
+	}
+	
+	private void init() {
+		setBackgroundColor(Color.GREEN);
+	}
 
 }
