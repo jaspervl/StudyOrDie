7e3df1d65fd5e7a52532e224e2d2b320066eba9e
jaspervl <hekbek@hotmail.nl>
2015-03-09 16:32:40 +0100

    Fragmention of large
    Fragment screen

--- StudyOrDie/StudyOrDie/gen/nl/glasbakheroes/StudyOrDie/R.java
+++ StudyOrDie/StudyOrDie/gen/nl/glasbakheroes/StudyOrDie/R.java
@@ -1642,7 +1642,7 @@ containing a value of this type.
     }
     public static final class id {
         public static final int LinearLayout1=0x7f09003f;
-        public static final int RelativeLayout1=0x7f090043;
+        public static final int RelativeLayout1=0x7f090044;
         public static final int action_bar=0x7f090031;
         public static final int action_bar_activity_content=0x7f090003;
         public static final int action_bar_container=0x7f090030;
@@ -1660,11 +1660,11 @@ containing a value of this type.
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
         public static final int btnBeatMe=0x7f090040;
-        public static final int btnDown=0x7f090047;
-        public static final int btnLeft=0x7f090045;
-        public static final int btnMenu=0x7f090049;
-        public static final int btnRight=0x7f090048;
-        public static final int btnUp=0x7f090046;
+        public static final int btnDown=0x7f090048;
+        public static final int btnLeft=0x7f090046;
+        public static final int btnMenu=0x7f09004a;
+        public static final int btnRight=0x7f090049;
+        public static final int btnUp=0x7f090047;
         public static final int checkbox=0x7f090029;
         public static final int collapseActionView=0x7f090015;
         public static final int decor_content_parent=0x7f09002f;
@@ -1676,13 +1676,14 @@ containing a value of this type.
         public static final int end=0x7f09001c;
         public static final int expand_activities_button=0x7f090022;
         public static final int expanded_menu=0x7f090028;
+        public static final int fragmentContainer=0x7f090041;
         public static final int home=0x7f090000;
         public static final int homeAsUp=0x7f09000e;
         public static final int icon=0x7f090026;
         public static final int ifRoom=0x7f090016;
         public static final int image=0x7f090023;
         public static final int listMode=0x7f09000a;
-        public static final int listView1=0x7f090041;
+        public static final int listView1=0x7f090042;
         public static final int list_item=0x7f090025;
         public static final int middle=0x7f09001d;
         public static final int never=0x7f090017;
@@ -1706,10 +1707,10 @@ containing a value of this type.
         public static final int showHome=0x7f090011;
         public static final int showTitle=0x7f090012;
         public static final int split_action_bar=0x7f090004;
-        public static final int studyOrDieGameBoardView1=0x7f090044;
+        public static final int studyOrDieGameBoardView1=0x7f090045;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000c;
-        public static final int textView1=0x7f090042;
+        public static final int textView1=0x7f090043;
         public static final int title=0x7f090027;
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
--- StudyOrDie/StudyOrDie/src/nl/glasbakheroes/StudyOrDie/game/MenuActivity.java
+++ StudyOrDie/StudyOrDie/src/nl/glasbakheroes/StudyOrDie/game/MenuActivity.java
@@ -1,17 +1,24 @@
 package nl.glasbakheroes.StudyOrDie.game;
 
-import android.app.Activity;
+import nl.glasbakheroes.StudyOrDie.R;
+import nl.glasbakheroes.StudyOrDie.game.menu.CharacterMenu;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
-public class MenuActivity extends Activity {
-	
-	
+public class MenuActivity extends FragmentActivity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-	
-		
+		setContentView(R.layout.activity_menu);
+
+		FragmentManager manager = getSupportFragmentManager();
+
+		CharacterMenu menu = new CharacterMenu();
+
+		manager.beginTransaction().add(R.id.fragmentContainer, menu).commit();
+
 	}
 
 }
