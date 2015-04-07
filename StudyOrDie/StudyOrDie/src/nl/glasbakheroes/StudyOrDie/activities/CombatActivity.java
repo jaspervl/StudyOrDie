f9474fa77d1e5b1c91f54b39d1bff7c3098d15c8
enjee <njvandepol@gmail.com>
2015-04-07 08:55:26 +0200

    end game fix

--- StudyOrDie/StudyOrDie/src/nl/glasbakheroes/StudyOrDie/activities/CombatActivity.java
+++ StudyOrDie/StudyOrDie/src/nl/glasbakheroes/StudyOrDie/activities/CombatActivity.java
@@ -133,7 +133,7 @@ public class CombatActivity extends Activity implements Observer {
 				Intent returnCoreIntent = new Intent(CombatActivity.this, CoreActivity.class);
 				returnCoreIntent.putExtra("result", "Dummy");
 				setResult(StudyOrDieGameBoard.REQUEST_COMBAT_INTENT, returnCoreIntent);
-				if (boss.getName().equals("Jan")) {
+				if (boss.getName().equals("Jan") && !boss.getAlive()) {
 					Intent gameFinishIntent = new Intent(CombatActivity.this, InformationActivity.class);
 					gameFinishIntent.putExtra("info", "win");
 					startActivity(gameFinishIntent);
